c570694b724b346520788de89ccae56cd7a53c0c
Suraj Rajput <suraj21071998@gmail.com>
2020-07-24 15:08:12 +0530

    Added address association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_29_122313) do
+ActiveRecord::Schema.define(version: 2020_07_24_093344) do
+
+  create_table "addresses", force: :cascade do |t|
+    t.string "city"
+    t.string "state"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_addresses_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.text "name"
@@ -19,4 +28,5 @@ ActiveRecord::Schema.define(version: 2020_06_29_122313) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "addresses", "users"
 end
